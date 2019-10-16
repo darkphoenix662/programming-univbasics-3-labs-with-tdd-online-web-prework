e5a663180d0e0dcf4f0fc3d0edbafd274238bd26
Sean Good <tree662@protonmail.com>
2019-10-16 00:14:24 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
-# Add your variables here
+def operation
+  puts "Type 1 to add, 2 to subtract, 3 to multiply, or 4 to divide 2 numbers"
+  
+  if operation == 1 
+    return "add"
+  elsif operation == 2
+    return "subract"
+  if operation ==3
+    return "multiply"
+  if operation == 4
+    return "divide"
